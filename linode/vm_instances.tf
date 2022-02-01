6b8dbcfeb787299933f842c335f08985c87c3775
KJone <58522694+KJone1@users.noreply.github.com>
2022-02-01 13:19:09 +0200

    Update alerts configuration
     + swap size change:
        512 mb -> 2024 mb

--- linode/vm_instances.tf
+++ linode/vm_instances.tf
@@ -6,10 +6,18 @@ resource "linode_instance" "tftest" {
   type            = "g6-standard-2"
   # public key for ssh # 
   authorized_keys = [data.linode_sshkey.ssh-Jan2022.ssh_key]
-  root_pass       = "bloodyvladi"
+  root_pass       = "testpasstfdemo"
   # group         = "group_test"
   tags            = ["tag_test1", "tag_test2"]
-  swap_size       = 512
+  swap_size       = 2048   # 2GB swap
   private_ip      = true
-  #  TODO: set up alerts configuration  
+  # alerts configuration # 
+  alerts {
+    cpu            = 90    # 90 %
+    io             = 10000 # 10k IOPS
+    network_in     = 10    # 10 Mb/s
+    network_out    = 10    # 10 Mb/s
+    transfer_quota = 80    # 80 %
+  } 
+  watchdog_enabled = true
 }
