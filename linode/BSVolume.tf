633d44813c7ede981b567684d8d15b27f4ae8177
KJone <58522694+KJone1@users.noreply.github.com>
2022-02-02 17:03:52 +0200

    Create BSVolume.tf

---
+++ linode/BSVolume.tf
@@ -0,0 +1,8 @@
+resource "linode_volume" "tftest" {
+    count     = var.deployment_count
+    label     = "BSV.${count.index + 1}"
+    region    = var.region
+    size      = 20 # in GB
+    linode_id = linode_instance.tftest[count.index].id
+    tags      = ["tagtest1","tagtest2"]
+}
