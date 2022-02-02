85be5b1bd69cf6353d5f95e859804e3e1146cd49
KJone <58522694+KJone1@users.noreply.github.com>
2022-02-02 17:04:09 +0200

    Add volume data output

--- linode/output.tf
+++ linode/output.tf
@@ -1,23 +1,19 @@
-# output "instance_1" {
-#   description = "Metadata of instance 1."
-#   value = [linode_instance.tftest[0].label,
-#           "public ipv4: ${linode_instance.tftest[0].ip_address}",
-#           "private ipv4: ${linode_instance.tftest[0].private_ip_address}",
-#           "location: ${data.linode_region.region_to_country.country}" ]
-# }
-# output "instance_2" {
-#   description = "Metadata of instance 2."
-#   value = [linode_instance.tftest[1].label,
-#           "public ipv4: ${linode_instance.tftest[1].ip_address}",
-#           "private ipv4: ${linode_instance.tftest[1].private_ip_address}",
-#           "location: ${data.linode_region.region_to_country.country}"]
-# }
 output "instance_metadata" {
-  description = "Metadata of deployed linode instances"
+  description = "Metadata of deployed Linode instances"
   value = {
     for instance in linode_instance.tftest:
         instance.label => {public_ipv4_addr = instance.ip_address,
                           private_ip_addr   = instance.private_ip_address,
                           deployed_location = data.linode_region.region_to_country.country}
+  }
+}
+output "volume_metadata" {
+  description = "Metadata of deployed Block Storage Volumes"
+  value = {
+    for volume in linode_volume.tftest:
+        volume.label => {path        = volume.filesystem_path,
+                        size         = volume.size,
+                        status       = volume.status,
+                        connected_to = volume.linode_id}
   }
 }
