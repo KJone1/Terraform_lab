077288105db4adcc3666caf21a72d990a59836c0
KJone <58522694+KJone1@users.noreply.github.com>
2022-02-03 14:20:13 +0200

    Update output.tf

--- linode/output.tf
+++ linode/output.tf
@@ -4,14 +4,15 @@ output "instance_metadata" {
     for instance in linode_instance.tftest:
         instance.label => {public_ipv4_addr = instance.ip_address,
                           private_ip_addr   = instance.private_ip_address,
-                          deployed_location = data.linode_region.region_to_country.country}
+                          deployed_location = data.linode_region.region_to_country.country,
+                          status            = instance.status}
   }
 }
 output "volume_metadata" {
   description = "Metadata of deployed Block Storage Volumes"
   value = {
     for volume in linode_volume.tftest:
-        volume.label => {path        = volume.filesystem_path,
+        volume.label => {disk_path   = volume.filesystem_path,
                         size         = volume.size,
                         status       = volume.status,
                         connected_to = volume.linode_id}
