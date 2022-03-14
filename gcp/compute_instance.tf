741b67521dfb2a33b6f8a6a934d5ea20cbc8059b
KJone <58522694+KJone1@users.noreply.github.com>
2022-03-14 20:51:01 +0200

    Add labels

--- gcp/compute_instance.tf
+++ gcp/compute_instance.tf
@@ -1,12 +1,19 @@
 
 resource "google_compute_instance" "default" {
   count          = var.deployment_count
-  name           = "tf_test.${count.index + 1}"
+  name           = "tf-test-${count.index + 1}"
   description    = "vm instance deployment test"
   machine_type   = "e2-micro"
   zone           = "europe-central2-c"
   desired_status = "RUNNING"
-  tags = ["foo", "bar"]
+  labels         = {
+    "version"     = 1.1,
+    "environment" = "prod",
+    "role"        = "web-server"
+    "app-name"    = "playground"
+    "region"      = "europe-central"
+    }
+  tags           = ["foo", "bar"]
   boot_disk {
     # ↓ On the instance, this device will be /dev/disk/by-id/google-{{device_name}}
     device_name = "playground"
