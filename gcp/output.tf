c83807fa0e7b983ab1496fb6b3abe41796bdeded
KJone <58522694+KJone1@users.noreply.github.com>
2022-04-01 18:47:53 +0300

    Fix output

--- gcp/output.tf
+++ gcp/output.tf
@@ -3,7 +3,7 @@ output "instance_metadata" {
   description = "Metadata of deployed gcp compute instance"
   value = {
     for instance in google_compute_instance.default:
-        instance.name => {deployed_image    = instance.boot_disk[*].initialize_params[*].image,
+        instance.name => {deployed_image    = instance.boot_disk[0].initialize_params[0].image,
                           deployed_zone     = instance.zone,
                           status            = instance.current_status}
   }
