ec19280777a9ca878e1753efccbd5a8f0470a38b
KJone <58522694+KJone1@users.noreply.github.com>
2022-04-02 11:55:37 +0300

    Add project and region variables

--- gcp/variables.tf
+++ gcp/variables.tf
@@ -1,7 +1,32 @@
 
+variable "project" {
+  description = "project id"
+  type        = map
+  nullable    = false
+  sensitive   = false
+  default     = {
+    test-project = "onyx-philosophy-333713"
+  }
+}
 variable "deployment_count" {
   description = "number of instances to deploy"
   type        = number
   nullable    = false
   sensitive   = false
+  default     = 1
+}
+variable "region" {
+  description = "region to deploy"
+  type        = map
+  nullable    = false
+  sensitive   = false
+  default     = {
+    poland      = "europe-central2"
+    finland     = "europe-north1"
+    belgium     = "europe-west1"
+    england     = "europe-west2"
+    germany     = "europe-west3"
+    switzerland = "europe-west6"
+       
+  } 
 }
