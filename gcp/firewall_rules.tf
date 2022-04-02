e6cba346174e0c5824f6eb02feaf725d5231a33a
KJone <58522694+KJone1@users.noreply.github.com>
2022-04-02 13:43:28 +0300

    Add new rules
    ssh, icmp, internal

--- gcp/firewall_rules.tf
+++ gcp/firewall_rules.tf
@@ -3,7 +3,7 @@
 # https://github.com/tasdikrahman/terraform-gcp-examples/blob/master/single-and-multi-node-compute-vm/firewall.tf
 
 resource "google_compute_firewall" "http-rule" {
-  name        = "webserver-firewall-rules"
+  name        = "vpc-allow-http"
   network     = google_compute_network.vpc_network.name
   description = "Firewall rule targeting tagged http web servers"
   allow {
@@ -14,7 +14,7 @@ resource "google_compute_firewall" "http-rule" {
   target_tags = ["http"]
 }
 resource "google_compute_firewall" "https-rule" {
-  name        = "webserver-firewall-rules"
+  name        = "vpc-allow-https"
   network     = google_compute_network.vpc_network.name
   description = "Firewall rule targeting tagged https web servers"
   allow {
@@ -23,4 +23,42 @@ resource "google_compute_firewall" "https-rule" {
   }
   source_ranges = ["0.0.0.0/0"]
   target_tags = ["https"]
+}
+resource "google_compute_firewall" "ssh-rule" {
+  name        = "vpc-allow-ssh"
+  network     = google_compute_network.vpc_network.name
+  description = "Firewall rule for ssh connection"
+  allow {
+    protocol  = "tcp"
+    ports     = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["ssh"]
+}
+resource "google_compute_firewall" "ping-rule" {
+  name        = "vpc-allow-icmp"
+  network     = google_compute_network.vpc_network.name
+  description = "Firewall rule for ping"
+  allow {
+    protocol  = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags = ["ping"]
+}
+resource "google_compute_firewall" "internal-rule" {
+  name        = "vpc-allow-internal"
+  network     = google_compute_network.vpc_network.name
+  description = "Firewall rule for internal communication"
+  allow {
+    protocol  = "tcp"
+    ports     = ["0-65535"]
+  }
+  allow {
+    protocol  = "udp"
+    ports     = ["0-65535"]
+  }
+  allow {
+    protocol  = "icmp"
+  }
+  source_ranges = ["10.128.0.0/9"]
 }
