00e1ce66d8dfd021f88a908fb9e2a24208680c1f
KJone <58522694+KJone1@users.noreply.github.com>
2022-04-08 11:47:20 +0300

    format spaces

--- gcp/block-storage.tf
+++ gcp/block-storage.tf
@@ -2,16 +2,16 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_disk
 
 resource "google_compute_disk" "app-data-storage" {
-  name  = "test-block"
-  description = "storage extantion for vm"
-  labels = {
+  name                      = "test-block"
+  description               = "storage extantion for vm"
+  size                      = 100  # in GB
+  physical_block_size_bytes = 4096
+  type                      = "pd-ssd"
+  zone                      = "${var.region["poland"]}-c"
+  labels                    = {
     "environment" = "prod",
     "role"        = "web-server-storage"
     "app-name"    = "playground"
     "region"      = "europe-central"
   }
-  size = 100  # in GB
-  physical_block_size_bytes = 4096
-  type  = "pd-ssd"
-  zone  = "${var.region["poland"]}-c"
 }
