9b93c9dc1e7377eefa5e5b09b9ed1d273cb57db0
KJone <58522694+KJone1@users.noreply.github.com>
2022-04-08 13:21:16 +0300

    format text

--- gcp/firewall-rules.tf
+++ gcp/firewall-rules.tf
@@ -3,52 +3,53 @@
 # https://github.com/tasdikrahman/terraform-gcp-examples/blob/master/single-and-multi-node-compute-vm/firewall.tf
 
 resource "google_compute_firewall" "http-rule" {
-  name        = "vpc-allow-http"
-  network     = google_compute_network.vpc_network.name
-  description = "Firewall rule targeting tagged http web servers"
+  name          = "vpc-allow-http"
+  network       = google_compute_network.vpc_network.name
+  description   = "Firewall rule targeting tagged http web servers"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http"]
   allow {
     protocol  = "tcp"
     ports     = ["80"]
   }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["http"]
 }
 resource "google_compute_firewall" "https-rule" {
-  name        = "vpc-allow-https"
-  network     = google_compute_network.vpc_network.name
-  description = "Firewall rule targeting tagged https web servers"
+  name          = "vpc-allow-https"
+  network       = google_compute_network.vpc_network.name
+  description   = "Firewall rule targeting tagged https web servers"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["https"]
   allow {
     protocol  = "tcp"
     ports     = ["443"]
   }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["https"]
 }
 resource "google_compute_firewall" "ssh-rule" {
-  name        = "vpc-allow-ssh"
-  network     = google_compute_network.vpc_network.name
-  description = "Firewall rule for ssh connection"
+  name          = "vpc-allow-ssh"
+  network       = google_compute_network.vpc_network.name
+  description   = "Firewall rule for ssh connection"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
   allow {
     protocol  = "tcp"
     ports     = ["22"]
   }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["ssh"]
 }
 resource "google_compute_firewall" "ping-rule" {
-  name        = "vpc-allow-icmp"
-  network     = google_compute_network.vpc_network.name
-  description = "Firewall rule for ping"
+  name          = "vpc-allow-icmp"
+  network       = google_compute_network.vpc_network.name
+  description   = "Firewall rule for ping"
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ping"]
   allow {
     protocol  = "icmp"
   }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags = ["ping"]
 }
 resource "google_compute_firewall" "internal-rule" {
-  name        = "vpc-allow-internal"
-  network     = google_compute_network.vpc_network.name
-  description = "Firewall rule for internal communication"
+  name          = "vpc-allow-internal"
+  network       = google_compute_network.vpc_network.name
+  description   = "Firewall rule for internal communication"
+  source_ranges = ["10.128.0.0/9"]
   allow {
     protocol  = "tcp"
     ports     = ["0-65535"]
@@ -60,5 +61,4 @@ resource "google_compute_firewall" "internal-rule" {
   allow {
     protocol  = "icmp"
   }
-  source_ranges = ["10.128.0.0/9"]
 }
