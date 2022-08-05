d181ad7d80a8859d768b1a84585e46ccabf132a3
KJone <58522694+KJone1@users.noreply.github.com>
2022-08-05 15:55:17 +0300

    Add instance name and use ami from data source.

--- aws/vm/ec2.tf
+++ aws/vm/ec2.tf
@@ -1,9 +1,12 @@
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
-
 resource "aws_instance" "defualt" {
   count         = var.deployment_count
-  ami           = "ami-004b161a1cceb1ceb" # us-east-1
+  ami           = data.aws_ami.rocky8-us-east1.id
   instance_type = "t3.small"
+  key_name      = "key-082022"
+  tags = {
+    Name = "srv-${count.index + 1}"
+  }
 }
