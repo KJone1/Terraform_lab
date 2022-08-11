8dd4a3f0892b3cfcd173f940cbdf585dea67a4af
KJone <58522694+KJone1@users.noreply.github.com>
2022-08-11 20:23:38 +0300

    Improve extra disk configuration.

--- gcp/vm/compute-instance.tf
+++ gcp/vm/compute-instance.tf
@@ -1,22 +1,39 @@
 
-module "network" {
-  source       = "../modules/network"
-  network_name = "my-main-vpc-network" 
-}
+# compute => https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance
+# disk    => https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_disk
 
-resource "google_compute_instance" "default" {
-  count          = var.deployment_count
-  name           = "tf-test-${count.index + 1}"
-  description    = "vm instance deployment test"
-  machine_type   = "e2-standard-4"
-  zone           = "${var.region}-${var.zone}"
-  desired_status = "RUNNING"
-  labels         = {
+locals {
+  base_name   = "khan"
+  labels = {
     "environment" = "dev",
     "role"        = "dev-server"
     "app"         = "playground"
     "region"      = var.region
   }
+}
+module "network" {
+  source       = "../modules/network"
+  network_name = "my-main-vpc-network" 
+}
+
+resource "google_compute_disk" "app-data-storage" {
+  count                     = var.deployment_count
+  name                      = "${local.base_name}-${count.index + 1}-disk"
+  description               = "storage extantion for vm"
+  size                      = 20  # in GB
+  physical_block_size_bytes = 4096
+  type                      = "pd-ssd"
+  zone                      = "${var.region}-${var.zone}"
+  labels                    = local.labels
+}
+resource "google_compute_instance" "default" {
+  count          = var.deployment_count
+  name           = "${local.base_name}-${count.index + 1}"
+  description    = "vm instance deployment test"
+  machine_type   = "e2-standard-4"
+  zone           = "${var.region}-${var.zone}"
+  desired_status = "RUNNING"
+  labels         = local.labels
   tags           = [
     "http",
     "https",
@@ -36,7 +53,7 @@ resource "google_compute_instance" "default" {
     }
   }
   attached_disk {
-    source        = google_compute_disk.app-data-storage.self_link
+    source        = google_compute_disk.app-data-storage[count.index].self_link
     device_name   = "playground-ext"
   }
   network_interface {
