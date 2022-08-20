fbfae514a9731fded4f61ba131af48a854dab4e9
KJone <58522694+KJone1@users.noreply.github.com>
2022-08-20 12:28:17 +0300

    Update output.tf

--- aws/modules/network/output.tf
+++ aws/modules/network/output.tf
@@ -7,7 +7,7 @@ output "subnet_id" {
   description = "VPC network name"
   value       = aws_subnet.first.id
 }
-output "sg_firewall_id" {
-  description = "vpc security group 'firewall' id"
-  value       = aws_security_group.firewall.id
+output "sg_firewall" {
+  description = "vpc security group 'firewall'"
+  value       = aws_security_group.firewall
 }
