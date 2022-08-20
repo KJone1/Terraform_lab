40601e992cec3928cd0a2f26ed1bbe4bd42e378a
KJone <58522694+KJone1@users.noreply.github.com>
2022-08-20 12:27:53 +0300

    Text Format.

--- aws/modules/network/vpc.tf
+++ aws/modules/network/vpc.tf
@@ -1,6 +1,7 @@
 
 # vpc    => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
 # subnet => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+
 resource "aws_vpc" "vpc_network" {
   cidr_block = "160.24.0.0/16"
   instance_tenancy = "default"
@@ -10,9 +11,9 @@ resource "aws_vpc" "vpc_network" {
 }
 
 resource "aws_subnet" "first" {
-  vpc_id     = aws_vpc.vpc_network.id
+  vpc_id            = aws_vpc.vpc_network.id
   availability_zone = var.availability_zone
-  cidr_block = "160.24.1.0/24"
+  cidr_block        = "160.24.1.0/24"
 
   tags = {
     Name = "First"
