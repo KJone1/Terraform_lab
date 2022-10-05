8b4c8dc938f9866b003e1adc97412f10ab394088
KJone <58522694+KJone1@users.noreply.github.com>
2022-10-05 12:19:35 +0300

    Add internet_gw and route table.

--- aws/modules/network/vpc.tf
+++ aws/modules/network/vpc.tf
@@ -1,6 +1,9 @@
 
-# vpc    => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
-# subnet => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+# vpc              => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+# subnet           => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+# internet_gateway => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/internet_gateway
+# route_table      => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
+# rt_association   => https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
 
 resource "aws_vpc" "vpc_network" {
   cidr_block = "160.24.0.0/16"
@@ -18,4 +21,22 @@ resource "aws_subnet" "first" {
   tags = {
     Name = "First"
   }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc_network.id
+  tags = {
+    Name = "internet_gw"
+  }
+}
+resource "aws_route_table" "toydeploy-rt" {
+  vpc_id = aws_vpc.vpc_network.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "toydeploy-rta" {
+  subnet_id      = aws_subnet.first.id
+  route_table_id = aws_route_table.toydeploy-rt.id
 }
