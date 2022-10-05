f8822cc34a19e462a90d32face977ccf136834eb
KJone <58522694+KJone1@users.noreply.github.com>
2022-10-05 12:18:54 +0300

    Update ami data source.

--- aws/vm/data-source.tf
+++ aws/vm/data-source.tf
@@ -1,12 +1,25 @@
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
 
-data "aws_ami" "rocky8-us-east1" {
-  owners = ["aws-marketplace"]
+data "aws_ami" "rhel-us-east1" {
+  owners = ["309956199498"]
   most_recent = true
   filter {
     name   = "name"
-    values = ["Rocky-8-ec2*"]
+    values = ["RHEL-8*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "amzlinux-us-east1" {
+  owners = ["137112412989"]
+  most_recent = true
+  filter {
+      name   = "name"
+      values = ["amzn-ami*"]
   }
   filter {
     name   = "architecture"
