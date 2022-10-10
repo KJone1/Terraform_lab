344002b09de4ac01c79afc0f8801c31368dcf261
KJone <58522694+KJone1@users.noreply.github.com>
2022-10-10 14:13:36 +0300

    Removed wg sg.

--- aws/modules/network/output.tf
+++ aws/modules/network/output.tf
@@ -7,13 +7,9 @@ output "subnet_id" {
   description = "VPC subnet id"
   value       = aws_subnet.first.id
 }
-output "sg_firewall" {
-  description = "VPC security group 'firewall'"
-  value       = aws_security_group.firewall
-}
-output "sg_wg_vpn" {
-  description = "VPC security group 'wg_vpn'"
-  value       = aws_security_group.wg_vpn
+output "sg_basic" {
+  description = "VPC security group 'basic'"
+  value       = aws_security_group.basic
 }
 output "internet_gw" {
   description = "VPC internet gatewey"
--- aws/modules/network/security-group.tf
+++ aws/modules/network/security-group.tf
@@ -1,7 +1,7 @@
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 
-resource "aws_security_group" "firewall" {
+resource "aws_security_group" "basic" {
   name        = "vpc_firewall"
   description = "VPC firewall rules"
   vpc_id      = aws_vpc.vpc_network.id
@@ -41,39 +41,3 @@ resource "aws_security_group" "firewall" {
     create_before_destroy = true
   }
  }
-
-resource "aws_security_group" "wg_vpn" {
-  name        = "wg_vpn_firewall"
-  description = "WireGuard VPN firewall rules"
-  vpc_id      = aws_vpc.vpc_network.id
-
-  
-  ingress {
-    description      = "Allow ssh"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  ingress {
-    description      = "Allow wg"
-    from_port        = 51820
-    to_port          = 51820
-    protocol         = "udp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description      = "Allow all egress"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-}
