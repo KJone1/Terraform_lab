bfddfd9871a4df1f5916f22ee57ff64943741d05
KJone <58522694+KJone1@users.noreply.github.com>
2022-10-10 14:05:58 +0300

    removed wg rule.

--- gcp/modules/network/firewall-rules.tf
+++ gcp/modules/network/firewall-rules.tf
@@ -84,21 +84,3 @@ resource "google_compute_firewall" "internal-rule" {
     protocol  = "icmp"
   }
 }
-resource "google_compute_firewall" "wg-vpn-rule" {
-  name          = "vpc-allow-wg-vpn"
-  network       = google_compute_network.vpc_network.name
-  description   = "Firewall rule for WireGuard VPN"
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["vpn"]
-  allow {
-    protocol  = "udp"
-    ports     = ["51820"]
-}
-  allow {
-    protocol  = "udp"
-    ports     = ["53"]
-}
-log_config {
-    metadata = "INCLUDE_ALL_METADATA"
-  }
-}
