4b6aeffb54b799b1ecb52c728ee946d4e2d747c0
KJone <58522694+KJone1@users.noreply.github.com>
2022-10-10 09:07:15 +0300

    Add new sg.

--- aws/modules/network/output.tf
+++ aws/modules/network/output.tf
@@ -11,3 +11,11 @@ output "sg_firewall" {
   description = "VPC security group 'firewall'"
   value       = aws_security_group.firewall
 }
+output "sg_wg_vpn" {
+  description = "VPC security group 'wg_vpn'"
+  value       = aws_security_group.wg_vpn
+}
+output "internet_gw" {
+  description = "VPC internet gatewey"
+  value       = aws_internet_gateway.gw
+}
--- aws/modules/network/security-group.tf
+++ aws/modules/network/security-group.tf
@@ -40,5 +40,40 @@ resource "aws_security_group" "firewall" {
   lifecycle {
     create_before_destroy = true
   }
+ }
+
+resource "aws_security_group" "wg_vpn" {
+  name        = "wg_vpn_firewall"
+  description = "WireGuard VPN firewall rules"
+  vpc_id      = aws_vpc.vpc_network.id
+
+  
+  ingress {
+    description      = "Allow ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "Allow wg"
+    from_port        = 51820
+    to_port          = 51820
+    protocol         = "udp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "Allow all egress"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
 }
