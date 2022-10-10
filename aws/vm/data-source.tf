0b23802ffbebaeda3e93714cd43f5926dbf06c49
KJone <58522694+KJone1@users.noreply.github.com>
2022-10-10 09:06:30 +0300

    Change ami to ubuntu.

--- aws/vm/data-source.tf
+++ aws/vm/data-source.tf
@@ -1,25 +1,11 @@
 
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
 
-data "aws_ami" "rhel-us-east1" {
-  owners = ["309956199498"]
+data "aws_ami" "ubuntu-us-east1" {
   most_recent = true
   filter {
-    name   = "name"
-    values = ["RHEL-8*"]
-  }
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-}
-
-data "aws_ami" "amzlinux-us-east1" {
-  owners = ["137112412989"]
-  most_recent = true
-  filter {
-      name   = "name"
-      values = ["amzn-ami*"]
+      name   = "image-id"
+      values = ["ami-0149b2da6ceec4bb0"]
   }
   filter {
     name   = "architecture"
