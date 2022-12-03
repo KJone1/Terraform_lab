a4c4e735ff9963a11064f7d66de66d877c54bc48
KJone <58522694+KJone1@users.noreply.github.com>
2022-12-03 14:51:27 +0200

    Add machine_type output.

--- gcp/modules/vm/output.tf
+++ gcp/modules/vm/output.tf
@@ -5,7 +5,8 @@ output "metadata" {
     for instance in google_compute_instance.default:
       trim(instance.name,var.name) => {
         name              = instance.name, 
-        id                = instance.id
+        id                = instance.id,
+        type              = instance.machine_type
         instance_id       = instance.instance_id,
         deployed_image    = instance.boot_disk[0].initialize_params[0].image,
         deployed_zone     = instance.zone
