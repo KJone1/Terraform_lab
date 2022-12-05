7fe60b272f9355b4f11c90316092ad7b0d886737
KJone <58522694+KJone1@users.noreply.github.com>
2022-12-05 20:28:43 +0200

    Update output.tf

--- gcp/vm/output.tf
+++ gcp/vm/output.tf
@@ -2,9 +2,7 @@
 output "instance_metadata" {
   description = "Metadata of deployed gcp compute instance"
   value = {
-    for instance in google_compute_instance.default:
-        instance.name => {deployed_image    = instance.boot_disk[0].initialize_params[0].image,
-                          deployed_zone     = instance.zone,
-                          type              = instance.machine_type}
+    for instance in module.vm[*].metadata.output:
+        instance.name => instance...
   }
 }
